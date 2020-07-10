20673ec8019b6431fd358fbaea3b7f4b7c6d4e4c
jacklmbrt07 <jacklmbrt@gmail.com>
2020-07-10 14:20:18 -0700

    successfully gathers data from api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 
 /*----- constants -----*/
+const baseURL = "https://pokeapi.co/api/v2/pokemon";
+
 /*----- app's state (variables) -----*/
+let pokemon, pokemonDetail;
+
 /*----- cached element references -----*/
+const $ulEl = $('.collection');
+
+
 /*----- event listeners -----*/
+$ulEl.on('click', handleClick);
 /*----- functions -----*/
+function handleClick(event) {
+
+}
+
+getPokemon();
+
+function getPokemon() {
+    $.ajax(baseURL).then(
+        function(data){
+            console.log(data);
+        }, 
+        function(error){
+            console.log(error);
+        });
+}
+
+
+//git push -u origin master at the end
