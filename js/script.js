675803e3656d5cdb873b14ccd766b37a8030b7d5
jacklmbrt07 <jacklmbrt@gmail.com>
2020-07-10 14:57:16 -0700

    adds data to DOM

--- js/script.js
+++ js/script.js
@@ -13,20 +13,35 @@ const $ulEl = $('.collection');
 $ulEl.on('click', handleClick);
 /*----- functions -----*/
 function handleClick(event) {
-
-}
+    console.log(event);
+};
 
 getPokemon();
 
-function getPokemon() {
+function getPokemon(event) {
     $.ajax(baseURL).then(
         function(data){
+            pokemon = data.results;
+            render(); // programatically renders the html
             console.log(data);
         }, 
         function(error){
             console.log(error);
         });
-}
+};
 
+function generateHTML() {
+    return pokemon.map(function(p) {
+        return `<li class="collection-item red-text">
+                    <div style="text-transform: capitalize;">${p.name}<span data-url="${p.url}" class="secondary-content blue-text">Detail</span>
+                    </div>
+                </li>`
+    });
+};
+
+function render() {
+    const html = generateHTML().join("");
+    $ulEl.html(html)
+};
 
 //git push -u origin master at the end
